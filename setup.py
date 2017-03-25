735f355cb6cf2110580ff5aacde962fe2f730cdc
Tarjei Husøy <git@thusoy.com>
2017-03-24 20:52:32 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='porridge',
+    version='0.1.0',
+    author='Tarjei Husøy',
+    author_email='git@thusoy.com',
+    url='https://github.com/thusoy/porridge',
+    description="Simple, strong and standardized keyed password storage",
+    packages=find_packages(),
+    install_requires=['argon2_cffi'],
+    extras_require={
+        'test': [
+            'pytest',
+            'watchdog',
+        ],
+    },
+    classifiers=[
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Security',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
